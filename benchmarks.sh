81bba15590331c929cadd1c56ae165ddb938e7f4
berkayinceisci <110044051+berkayinceisci@users.noreply.github.com>
2025-10-20 08:21:36 +0300

    Update benchmarks.sh

--- benchmarks.sh
+++ benchmarks.sh
@@ -5,10 +5,26 @@
 sudo chown -R $USER /tdata
 
 cd /tdata
+
+# linux kernel, perf
+# Define the repository URL
+REPO_URL="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
+LATEST_TAG=$(git ls-remote --tags --sort='v:refname' $REPO_URL | \
+             grep -v 'rc' | \
+             grep -v '{}' | \
+             tail -n1 | \
+             cut -d'/' -f3)
+git clone --depth 1 --branch $LATEST_TAG $REPO_URL
+cd linux/tools/perf
+make -j$(nproc)
+cd -
+
+# gapbs
 git clone https://github.com/sbeamer/gapbs.git
 cd gapbs
 make
 make bench-graphs GRAPH_DIR=/tdata/graphs RAW_GRAPH_DIR=/tdata/graphs/raw
+
 cd ~/cloudlab
 
 echo "Benchmarks are installed"
