36f9ea62726f1f285521ae4d4320b23c02ba5a20
berkayinceisci <inceisciberkay@gmail.com>
2025-11-06 10:41:03 -0500

    install stow before setting dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,18 @@
+# stow
+if ! command -v stow &>/dev/null; then
+    echo "Installing stow..."
+    wget https://ftp.gnu.org/gnu/stow/stow-2.3.1.tar.gz
+    tar -xzf stow-2.3.1.tar.gz
+    cd stow-2.3.1
+    ./configure --prefix=$HOME/.local
+    make
+    make install
+    cd -
+    rm -rf stow-2.3.1
+else
+    echo "stow already installed, skipping..."
+fi
+
 # dotfiles
 if [ ! -d "$HOME/dotfiles" ]; then
     echo "Setting up dotfiles..."
