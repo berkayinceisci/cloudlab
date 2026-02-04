6bd5b3cd04f526a42ead664a815368e8acd96f22
berkayinceisci <110044051+berkayinceisci@users.noreply.github.com>
2026-02-04 02:40:41 -0500

    Update root_packages.sh

--- root_packages.sh
+++ root_packages.sh
@@ -10,7 +10,7 @@ sudo apt install -y -o Dpkg::Options::="--force-confnew" \
     zsh \
     vim \
     htop cpuid \
-    jq progress\
+    jq progress \
     numactl libnuma-dev \
     hwloc \
     msr-tools \
