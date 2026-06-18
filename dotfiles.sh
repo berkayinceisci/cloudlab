8db9dd5d30ad09e48993925ca84503f7bcff95bc
berkayinceisci <inceisciberkay@gmail.com>
2026-06-18 11:39:32 -0400

    rename install.sh -> bootstrap.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -25,7 +25,7 @@ if [ ! -d "$HOME/dotfiles" ]; then
 	echo "Setting up dotfiles..."
 	git clone git@github.com:berkayinceisci/dotfiles.git $HOME/dotfiles
 	cd $HOME/dotfiles
-	if ! ./install.sh; then
+	if ! ./bootstrap.sh; then
 		echo "stow failed, removing dotfiles directory..."
 		cd - >/dev/null
 		rm -rf $HOME/dotfiles
